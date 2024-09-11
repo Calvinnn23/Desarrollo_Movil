274a9b254b112ab54c55c5397ba4001c26ce6097
Calvin <creativomiel@gmail.com>
2024-09-10 20:53:18 -0500

    Entrega 2 Complete

---
+++ Entrega 2/app/src/main/java/com/example/proyect_1/MainActivity2.java
@@ -0,0 +1,97 @@
+package com.example.proyect_1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private TextView labelUser;
+    private String TAG = "Test";
+    private Button btnResp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.d(TAG, "Estoy en el onCreate");
+
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "Estoy en el onStart");
+
+        labelUser = findViewById(R.id.label_userName);
+        btnResp = findViewById(R.id.btnEnviar);
+
+        String name = getIntent().getStringExtra("name");
+        labelUser.setText(String.format("Hola, %s bien o qué, vamos a tomar polita mi fafa?", name));
+
+        btnResp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calendarEvent("Calendario", "Placita", 2000l, 6000l);
+            }
+        });
+    }
+
+    private void calendarEvent(String title, String location, long begin, long end) {
+        Intent intent = new Intent(Intent.ACTION_INSERT)
+                .setData(CalendarContract.Events.CONTENT_URI)
+                .putExtra(CalendarContract.Events.TITLE, title)
+                .putExtra(CalendarContract.Events.EVENT_LOCATION, location)
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, begin)
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, end);
+        if (intent.resolveActivity(getPackageManager()) == null) {
+            startActivity(intent);
+        }
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "Estoy en el onResume");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "Estoy en el onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "Estoy en el onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "Estoy en el onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "Estoy en el onRestart");
+    }
+}
